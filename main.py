4a3e6203c17a21f22e515b5e1f46a84776786dff
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-01-11 02:07:43 -0500

    window sizing

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from direct.showbase.ShowBase import ShowBase
+from panda3d.core import WindowProperties
 
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
+
+        # Set windows size
+        properties = WindowProperties()
+        properties.setSize(900, 800)
+        self.win.requestProperties(properties)
+
+        # disable mouse-based camera-control
+        self.disableMouse()
 
 game = Game()
 game.run()
