f8e11edfcc815efefdcb38dccab2396562f60175
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-01-16 02:16:57 -0500

    Create parse_asm.py

---
+++ parse_asm.py
@@ -0,0 +1,9 @@
+class Parse:
+	def __init__(self, code, code_type):
+    	self.code = code
+    	self.code_type = code_type
+
+	def get_block(code):
+		# print source code file to string
+
+		# parse code and render shapes
