f3d6b71714e70175f921b24513aceb82edae0bcd
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-01-16 02:19:50 -0500

    Update parse_asm.py

--- parse_asm.py
+++ parse_asm.py
@@ -1,9 +1,14 @@
+
 class Parse:
 	def __init__(self, code, code_type):
     	self.code = code
     	self.code_type = code_type
 
-	def get_block(code):
+    # parses code and renders blocks to screen
+	def blocks(obj):
 		# print source code file to string
 
 		# parse code and render shapes
+
+	# parses code and renders text explanation
+	def text(obj)
