143416b07b72870dcb84c30224b780fcdb6285a4
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-01-17 02:49:37 -0500

    working on buttons

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ background_position=[0,0]
 screen = pygame.display.set_mode((800, 800))
 pygame.display.set_caption("StackAsm")
 bg = pygame.image.load("forest.jpg")
+ebutton = pygame.image.load("easy.jpg")
+ebutton = pygame.transform.scale(ebutton, (80, 80))
 running = True
 
 while running:
+	#setup button position
+	x = 800 * 0.45
+	y = 800 * 0.4
+	x_change = 0
+
 	# Check for input
 	for event in pygame.event.get():
 		# close program if x clicked
@@ -17,6 +24,7 @@ while running:
 			running = False
 
 	screen.blit(bg, background_position)
+	screen.blit(ebutton, (x, y))
 	pygame.display.update()
 
 
