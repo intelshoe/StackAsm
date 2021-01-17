52128d08d2e716395a55002f49fd1766a4c7a9a9
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-01-17 02:33:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ pygame.init()
 
 background_position=[0,0]
 screen = pygame.display.set_mode((800, 800))
+pygame.display.set_caption("StackAsm")
 bg = pygame.image.load("forest.jpg")
-button = pygame.image.load("easy.png")
 running = True
 
 while running:
@@ -15,16 +15,10 @@ while running:
 		# close program if x clicked
 		if event.type == pygame.QUIT:
 			running = False
-		if event.type == pygame.MOUSEBUTTONDOWN:
-			
 
-	# Update game elements
-
-	# Call function to parse Assembly input
-	# renders blocks and text as needed
-
-	# Draw and show game surface
 	screen.blit(bg, background_position)
 	pygame.display.update()
+
+
 
 pygame.quit()
