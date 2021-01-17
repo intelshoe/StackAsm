ba4b63cca0856872ae9026654e6274e75aa908ee
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-01-16 20:55:30 -0500

    Create parse.py
    parse assembly code

---
+++ parse.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Assmebly Parser 
+returns commented code explaining what asm is doing
+
+Author: mellowpuppy
+"""
+import sys
+
+# read path to file + name from command line
+domain = sys.argv[1]
+asm_file = ""
+
