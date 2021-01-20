caa2856e134982fa99034f872c4292a4adb79e1f
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-01-20 00:10:47 -0500

    optimize draw func

--- button.py
+++ button.py
@@ -3,12 +3,10 @@ import os
 
 class Button:
 	'''
-	Makes a button class representing a piece of Assembly
+	Makes a button list representing Assembly
 	'''
 	def __init__(self, screen):
 		self.button1 = pygame.image.load(os.path.join("assets\\buttons", "blockhead.jpg"))
-		self.button1 = pygame.transform.scale(self.button1, (80, 80))
-		self.button1.convert()
 		self.screen = screen
 
 	def draw(self):
@@ -19,6 +17,8 @@ class Button:
 		"""
 		buttonY = 0
 		for x in range(3):
+			self.button1 = pygame.transform.scale(self.button1, (80, 80))
+			self.button1.convert()
 			self.screen.blit(self.button1, (0,buttonY))
 			buttonY += 80
 
